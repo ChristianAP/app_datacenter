391067aa1827946692de247ecf2c87a046342aed
Christian <christianatencio@upeu.edu.pe>
2022-07-16 20:19:31 -0500

    database actualizado

--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 import { Pool } from 'pg'
 
 export const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'root',
-    database: 'personal-app',
+    host: 'ec2-3-219-52-220.compute-1.amazonaws.com',
+    user: 'rrjxsqupfjntgw',
+    password: '45ec0a83e3660822b9e224bfbaf785ed166af2520f29adbe492378f387372d69',
+    database: 'd9fa3eq467bk6u',
     port: 5432,
-    // ssl: { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false }
 })
